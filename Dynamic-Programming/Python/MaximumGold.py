a428ea77e96d2420e996882355c78f0dc59382f0
Daniel Mejia <Daniel.Mejia2@Honeywell.com>
2022-03-03 21:47:45 -0600

    modified Maximum gold

--- Dynamic-Programming/Python/MaximumGold.py
+++ Dynamic-Programming/Python/MaximumGold.py
@@ -2,7 +2,7 @@ def maximum_gold(W, ws):
     values = [[0 for i in range(0, W + 1)] for i in range(0, len(ws) + 1)]
     for i in range(1, len(ws) + 1):
         for j in range(1, W + 1):
-            if ws[i - 1] < j:
+            if ws[i - 1] <= j:
                 values[i][j] = max(ws[i - 1] + values[i - 1][j - ws[i - 1]], values[i - 1][j])
             else:
                 values[i][j] = values[i - 1][j]
